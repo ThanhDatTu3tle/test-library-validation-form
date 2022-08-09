2b882d3a7518443651dd5cd8391b5095c9b50917
MỸ NGÂN <myngan@MacBook-Air-cua-MY-NGAN.local>
2022-08-09 18:39:05 +0700

    update code

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import { Validator } from './node_modules/auto-alidation-form/validator.js';
+// var Validator = require('./node_modules/auto-alidation-form/validator.js');
 
 // sử dụng thư viện
 // Validator('id của form cần validate', 'options') {} *id của form cần validate được hiểu là formSelector
 
-var form =  new Validator('#register-form', '.ngu', '.form-message'); // nhận lại đối tượng 'form' khi khởi tạo constructor Validator
+var form =  new Validator('#register-form', '.form-group', '.form-message'); // nhận lại đối tượng 'form' khi khởi tạo constructor Validator
 console.log(form);
 form.onSubmit = (formData) => { // đối tượng 'form' dùng để call API
     console.log(formData);
